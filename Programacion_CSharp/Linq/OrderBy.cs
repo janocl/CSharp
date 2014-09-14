bdb8b592f21dda7b1353141b8e7914b751fa5bed
janocl <juan.garces@outlook.com>
2014-09-14 13:51:09 -0300

    Agregada la clase donde explica el uso de "OrderBy" para LINQ

---
+++ Programacion_CSharp/Linq/OrderBy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacion_CSharp.Linq
+{
+    class OrderBy
+    {
+        public static void queryOrderBy()
+        {
+            var query = from p in Product.Show()
+                        where p.id > 0
+                        orderby p.marca ascending
+                        select p;
+
+            foreach (var item in query)
+            {
+                Console.WriteLine("{0}", item.marca);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
