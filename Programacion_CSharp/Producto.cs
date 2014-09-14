d3374f2d91adecc12994a49c627016f3b908b9ba
janocl <juan.garces@outlook.com>
2014-09-14 18:51:29 -0300

    Fix en la clase

---
+++ Programacion_CSharp/Producto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacion_CSharp
+{
+    public static class Producto
+    {
+        public class Productos
+        {
+            public int IdProducto { get; set; }
+            public string Marca { get; set; }
+            public string Tipo { get; set; }
+            public int Valor { get; set; }
+            public int Stock { get; set; }
+            public int IdProveedor { get; set; }
+        }
+
+        public static List<Productos> Show()
+        {
+            List<Productos> listaProductos = new List<Productos>() 
+            { 
+                new Productos{ IdProducto=1, Marca="Colun", Tipo="Leche", Stock=25, Valor=850, IdProveedor=4 },
+                new Productos{ IdProducto=2, Marca="Pepsi", Tipo="Bebida", Stock=45, Valor=1300, IdProveedor=2 },
+                new Productos{ IdProducto=3, Marca="Lays", Tipo="Papas fritas", Stock=53, Valor=1890, IdProveedor=3 },
+                new Productos{ IdProducto=4, Marca="Kunstmann", Tipo="Cerveza", Stock=322, Valor=5050, IdProveedor=2 },
+                new Productos{ IdProducto=5, Marca="Carozzi", Tipo="Spaghetti", Stock=500, Valor=1599, IdProveedor=5 },
+                new Productos{ IdProducto=6, Marca="Coca-Cola", Tipo="Bebida", Stock=450, Valor=1620, IdProveedor=2 },
+                new Productos{ IdProducto=7, Marca="Quillayes", Tipo="Yoghurt", Stock=100, Valor=2290, IdProveedor=1 },
+                new Productos{ IdProducto=8, Marca="Colun", Tipo="Yoghurt", Stock=200, Valor=1030, IdProveedor=1 },
+                new Productos{ IdProducto=9, Marca="Vital", Tipo="Agua Mineral", Stock=88, Valor=850, IdProveedor=5 },
+                new Productos{ IdProducto=10, Marca="Kross", Tipo="Cerveza", Stock=145, Valor=1890, IdProveedor=2 },
+                new Productos{ IdProducto=11, Marca="Fanta", Tipo="Bebida", Stock=105, Valor=1340, IdProveedor=3 },
+            };
+
+            return listaProductos;
+        }
+
+    }
+}
