aa2ec577a407755941e04e8bcebb5157ff24b186
janocl <janocl84@gmail.com>
2014-10-25 13:06:17 -0300

    Consulta que explica el uso de Join pero de una manera avanzada

---
+++ Programacion_CSharp/Linq/JoinAdvanced.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacion_CSharp.Linq
+{
+    class JoinAdvanced
+    {
+        public static void queryJoinAdvanced()
+        {
+            var query = from p in Producto.Show()
+                        join a in Proveedor.Show()
+                        on p.IdProducto equals a.IdProveedor
+                        select new { id = p.IdProducto, tipo = p.Tipo, 
+                                    valor = p.Valor, proveedor = a.Nombre, 
+                                    fono = a.Telefono };
+
+
+            foreach (var item in query)
+            {
+                Console.WriteLine("{0}", item);
+            }
+            Console.ReadKey();
+    }
+}
