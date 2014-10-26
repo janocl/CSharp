6a7bd054bd661a0eb48320ed61df27da93b8ae18
janocl <janocl84@gmail.com>
2014-10-26 14:17:24 -0300

    Consulta que explica el uso de Join con subconsultas

--- Programacion_CSharp/Linq/JoinAdvanced.cs
+++ Programacion_CSharp/Linq/JoinAdvanced.cs
@@ -6,16 +6,46 @@ using System.Threading.Tasks;
 
 namespace Programacion_CSharp.Linq
 {
-    class JoinAdvanced
+    public class JoinAdvanced
     {
         public static void queryJoinAdvanced()
         {
-            var query = from p in Producto.Show()
-                        join a in Proveedor.Show()
-                        on p.IdProducto equals a.IdProveedor
-                        select new { id = p.IdProducto, tipo = p.Tipo, 
-                                    valor = p.Valor, proveedor = a.Nombre, 
-                                    fono = a.Telefono };
+            var query = from proveedor in Proveedor.Show()
+                        join producto in Producto.Show()
+                        on proveedor.IdProveedor equals producto.IdProducto
+                        group proveedor by new
+                        {
+                            proveedor.IdProveedor,
+                            proveedor.Nombre,
+                            proveedor.Telefono
+                        } into g1
+                        select new
+                        {
+                            provId = g1.Key.IdProveedor,
+                            provNombre = g1.Key.Nombre,
+                            provTelefono = g1.Key.Telefono,
+                            ListaPedidos = from producto in Producto.Show()
+                            join pedido in Pedido.Show()
+                            on producto.IdProducto equals pedido.IdPedido
+                            group pedido by new
+                            {
+                                pedido.IdPedido,
+                                pedido.Fecha,
+                                pedido.Destinatario,
+                                pedido.Cantidad
+                            } into g2
+
+                            select new {
+                                provId = g1.Key.IdProveedor,
+                                provNombre = g1.Key.Nombre,
+                                provTelefono = g1.Key.Telefono,
+                                pedId = g2.Key.IdPedido,
+                                pedFecha = g2.Key.Fecha,
+                                pedDestinatario = g2.Key.Destinatario,
+                                pedCantidad = g2.Key.Cantidad
+                            }
+                        };
+
 
 
             foreach (var item in query)
@@ -23,5 +53,6 @@ namespace Programacion_CSharp.Linq
                 Console.WriteLine("{0}", item);
             }
             Console.ReadKey();
+        }
     }
 }
