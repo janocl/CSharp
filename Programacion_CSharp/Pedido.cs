18f7bd9a8dd73ec767372c27cbc80d5994c7729b
janocl <janocl84@gmail.com>
2015-03-24 22:59:47 -0300

    Arreglado problema en la clase Pedido al crear la lista de pedidos

--- Programacion_CSharp/Pedido.cs
+++ Programacion_CSharp/Pedido.cs
@@ -19,14 +19,14 @@ namespace Programacion_CSharp
 
         public static List<Pedidos> Show()
         {
-            List<Pedidos> listaPedidos = new List<Pedidos>();
+            List<Pedidos> listaPedidos = new List<Pedidos>()
             {
-                new Pedidos { IdPedido = 1, Fecha = new DateTime(2014, 05, 02), Destinatario = "Isamel Jara", Cantidad = 3, IdProducto = 5 };
-                new Pedidos { IdPedido = 2, Fecha = new DateTime(2014, 09, 09), Destinatario = "Jose Soto", Cantidad = 12, IdProducto = 3 };
-                new Pedidos { IdPedido = 3, Fecha = new DateTime(2014, 03, 22), Destinatario = "David Maldonado", Cantidad = 24, IdProducto = 7 };
-                new Pedidos { IdPedido = 4, Fecha = new DateTime(2014, 11, 19), Destinatario = "Sebastian Gonzales", Cantidad = 4, IdProducto = 10 };
-                new Pedidos { IdPedido = 5, Fecha = new DateTime(2014, 07, 12), Destinatario = "Pedro Martinez", Cantidad = 8, IdProducto = 1 };
-                new Pedidos { IdPedido = 6, Fecha = new DateTime(2014, 05, 07), Destinatario = "Angel Vega", Cantidad = 15, IdProducto = 11 };
+                new Pedidos { IdPedido = 1, Fecha = new DateTime(2014, 05, 02), Destinatario = "Isamel Jara", Cantidad = 3, IdProducto = 5 },
+                new Pedidos { IdPedido = 2, Fecha = new DateTime(2014, 09, 09), Destinatario = "Jose Soto", Cantidad = 12, IdProducto = 3 },
+                new Pedidos { IdPedido = 3, Fecha = new DateTime(2014, 03, 22), Destinatario = "David Maldonado", Cantidad = 24, IdProducto = 7 },
+                new Pedidos { IdPedido = 4, Fecha = new DateTime(2014, 11, 19), Destinatario = "Sebastian Gonzales", Cantidad = 4, IdProducto = 10 },
+                new Pedidos { IdPedido = 5, Fecha = new DateTime(2014, 07, 12), Destinatario = "Pedro Martinez", Cantidad = 8, IdProducto = 1 },
+                new Pedidos { IdPedido = 6, Fecha = new DateTime(2014, 05, 07), Destinatario = "Angel Vega", Cantidad = 15, IdProducto = 11 },
             };
 
             return listaPedidos;
