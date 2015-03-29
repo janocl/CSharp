9db31dd0ea2d2cf2c5ed3f6dc3f853d1c837c744
janocl <janocl84@gmail.com>
2015-03-28 23:59:47 -0300

    Agregada la consulta SelectMany

---
+++ Programacion_CSharp/Linq/SelectMany.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacion_CSharp.Linq
+{
+    public class SelectMany
+    {
+        public static void querySelectMany()
+        {
+
+            query = 
+
+
+
+
+            Console.WriteLine("\nDistinct : Este operador sirve para eliminar elementos duplicados de una secuencia. \n");
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
