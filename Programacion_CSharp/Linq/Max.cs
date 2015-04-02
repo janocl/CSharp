394dc85e86b491a7c6c55b67c6d4421fd9a1f220
janocl <janocl84@gmail.com>
2015-04-01 23:48:27 -0300

    WIP: Corrigiendo metodo Max()

--- Programacion_CSharp/Linq/Max.cs
+++ Programacion_CSharp/Linq/Max.cs
@@ -11,13 +11,12 @@ namespace Programacion_CSharp.Linq
         public static void queryMax()
         {
 
-            var query = from p in Producto.Show()
-                        let max = Producto.Show().Select(x => x.Stock).Max()
-                        select new { Marca = p.Marca, Stock = max };
+            var query = Producto.Show()
+                        .GroupBy(s=>s.Marca)
+                            .Select(s => new { Marca = s.Key,
+                                                Stock = s.Max(x=>x.Stock)}).ToList();
 
             Console.WriteLine("\nMax : El operador Max devuelve el máximo valor de una colección de registros. \n");
-            //Console.WriteLine("El producto con mayor stock:> {0}", query);
-            //Console.ReadKey();
             foreach (var item in query)
             {
                 Console.WriteLine(item);
