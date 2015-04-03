65419e20f6ad05fdddf68857a66cd9d5fdfb13bb
janocl <janocl84@gmail.com>
2015-04-03 10:19:44 -0300

    WIP: Agregando la clausula Single de LINQ

---
+++ Programacion_CSharp/Linq/Single.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programacion_CSharp.Linq
+{
+    public class Single
+    {
+        public static void singleQuery()
+        { 
+        
+        }
+
+    }
+}
